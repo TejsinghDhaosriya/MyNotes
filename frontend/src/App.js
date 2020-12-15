68f38d2dd7226202c06c700f9a0dcd733bfaeeba
TejsinghDhaosriya <tejsinghdhaosriya@gmail.com>
2020-12-15 19:16:23 +0530

    ListNotes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,49 @@
 import React ,{Component} from 'react';
 import {Button,Container,Row,Col} from 'reactstrap';
+import ListNotes from './components/ListNotes';
 
+let notes_temp=[
+  {
+    'id':1,
+    "title":'This is react note data',
+    'content':'This is the content'
+  },
+  {
+    'id':2,
+    "title":'My second ntoe',
+    'content':'This is the content'
+  },
+  {
+    'id':3,
+    "title":'3 rd note ',
+    'content':'This is  3 the content'
+  },
+]
 
 class App extends Component
 {
 constructor(props){
   super(props);
   this.state={
-    notes:[],
+    notes:notes_temp,
     current_note_id:0,
     is_creating:true,
 
   }
+
+  this.handleItemClick=this.handleItemClick.bind(this);
+  this.handleAddNote=this.handleAddNote.bind(this);
+}
+handleItemClick(id){
+  this.setState((prevState)=>{
+    return {is_creating:false,current_note_id:id}
+  })
+}
+handleAddNote(){
+  this.setState((prevState)=>{
+    return{is_creating:true}
+  })
+
 }
 
 render() {
@@ -23,15 +55,18 @@ render() {
           <h2>Realtime notes</h2>
         </Col>
         <Col xs="2">
-          <Button color="primary">Create a new Note</Button>
+          <Button color="primary" onClick={this.handleAddNote}>Create a new Note</Button>
         </Col>
       </Row>
       <Row>
         <Col xs="4">
-          <h5>List our notes here...</h5>
-        </Col>
+          <ListNotes notes={this.state.notes} handleItemClick={(id)=>this.handleItemClick(id)}/>
+          </Col>
         <Col xs="8">
           <p>Content/Editing here.</p>
+          {
+            this.state.is_creating ? "Creating now..." :`Editing note with id : ${this.state.current_note_id}`
+          }
         </Col>
       </Row>
 
